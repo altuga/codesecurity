0dfb19318b85f9f65a6022636258e1c5cf1c6fb4
Altuğ Bilgin Altıntaş <altuga@gmail.com>
2025-12-23 12:06:00 +0300

    Fix post-login redirect by mapping / to /welcome

--- section1/springsecsection1/src/main/java/org/jugistanbul/springsecsection1/controller/WelcomeController.java
+++ section1/springsecsection1/src/main/java/org/jugistanbul/springsecsection1/controller/WelcomeController.java
@@ -1,10 +1,21 @@
 package org.jugistanbul.springsecsection1.controller;
 
+import java.net.URI;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<Void> root() {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create("/welcome"))
+                .build();
+    }
 
     @GetMapping("/welcome")
     public  String sayWelcome () {
